4609b72c9c3e7e853e36f667b4bfd4346d859520
Mohammed <mouhamedrherbaoui@gmail.com>
2026-05-06 11:22:34 +0200

    ajout de flask-cors pour que hamza puisse appeler ma base de donnée et mes api directement grace a mon ip

--- app.py
+++ app.py
@@ -7,17 +7,24 @@ from alertes_auto import generer_alertes
 from routes.dashboard import dashboard_bp
 
 
+from flask import Flask
+from flask_cors import CORS
+from routes.parcelles import parcelles_bp
+from routes.meteo import meteo_bp
+from routes.alertes import alertes_bp
+from routes.observations import observations_bp
+from routes.dashboard import dashboard_bp
+from alertes_auto import generer_alertes
+
 app = Flask(__name__)
 app.secret_key = "agri_secret_key"
-app.register_blueprint(dashboard_bp)
+CORS(app)  # ajoute cette ligne
+
 app.register_blueprint(parcelles_bp)
 app.register_blueprint(meteo_bp)
 app.register_blueprint(alertes_bp)
 app.register_blueprint(observations_bp)
-
-@app.route("/")
-def index():
-    return "API Agri OK"
+app.register_blueprint(dashboard_bp)
 
 if __name__ == "__main__":
     generer_alertes()
