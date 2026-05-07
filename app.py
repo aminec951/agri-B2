eb92a2070c3788b4ca94909b5b4c80de6fb2b566
Mohammed <mouhamedrherbaoui@gmail.com>
2026-05-07 12:25:27 +0200

    mise a jour de app.py amine recupere se fichier et lance la vm

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from flask import Flask
-from routes.parcelles import parcelles_bp
-from routes.meteo import meteo_bp
-from routes.alertes import alertes_bp
-from routes.observations import observations_bp
-from alertes_auto import generer_alertes
-from routes.dashboard import dashboard_bp
-
-
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from routes.parcelles import parcelles_bp
 from routes.meteo import meteo_bp
@@ -18,7 +9,7 @@ from alertes_auto import generer_alertes
 
 app = Flask(__name__)
 app.secret_key = "agri_secret_key"
-CORS(app)  # ajoute cette ligne
+CORS(app)
 
 app.register_blueprint(parcelles_bp)
 app.register_blueprint(meteo_bp)
@@ -26,6 +17,22 @@ app.register_blueprint(alertes_bp)
 app.register_blueprint(observations_bp)
 app.register_blueprint(dashboard_bp)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/parcelles')
+def parcelles():
+    return render_template('parcelles.html')
+
+@app.route('/observations')
+def observations():
+    return render_template('observations.html')
+
+@app.route('/alertes')
+def alertes():
+    return render_template('alertes.html')
+
 if __name__ == "__main__":
     generer_alertes()
-    app.run(debug=True, host="0.0.0.0",port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5001)
